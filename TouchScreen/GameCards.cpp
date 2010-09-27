ee0024b7e162ae5b63223b8856e30d739d296f3d
unknown <James@.(none)>
2010-09-27 12:39:12 +0200

    Touchscreen removed ;

--- TouchScreen/GameCards.cpp
+++ TouchScreen/GameCards.cpp
@@ -21,7 +21,7 @@ MAUIMoblet::MAUIMoblet() {
 	MAExtent screenSize = maGetScrSize();
 	scrWidth = EXTENT_X(screenSize);
 	scrHeight = EXTENT_Y(screenSize);
-;
+
 	feed = Feed();
 	feed.setAll(getData(FEED));
 	feed.setAlbum(getData(ALBUM));
