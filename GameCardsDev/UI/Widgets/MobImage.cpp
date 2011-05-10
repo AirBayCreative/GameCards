e8589b345e2dbd8dbd791c5a85cee941310dedc3
heinzs <heinz@mytcg.net>
2011-05-10 14:48:54 +0200

    Overlay scaling fixed

--- GameCardsDev/UI/Widgets/MobImage.cpp
+++ GameCardsDev/UI/Widgets/MobImage.cpp
@@ -33,13 +33,8 @@ bool MobImage::statContains(int x, int y, int width, int height, int pointX, int
 void MobImage::selectStat(int x, int y, int width, int height){
 	Gfx_setColor(0,0,0);
 	Gfx_clearMatrix();
-	//lprintfln("x %i",x);
-	//lprintfln("this->getWidth() %i",this->getWidth());
-	//lprintfln("imageWidth %i",imageWidth);
-	//lprintfln("(imageWidth>>1) %i",(imageWidth>>1));
 	//drawRectangle((x*this->getWidth()/250),(y*this->getHeight()/350),width*this->getWidth()/250,height*this->getHeight()/350);
-	drawRectangle((x*imageWidth/250),(y*this->getHeight()/350),width*imageWidth/250,height*this->getHeight()/350);
-	//Gfx_fillRect((x*this->getWidth()/250),(y*this->getHeight()/350),width*this->getWidth()/250,height*this->getHeight()/350);
+	drawRectangle((5 + (paddedBounds.width >> 1) - (imageWidth >> 1))+(x*imageWidth/250),((paddedBounds.height >> 1) - (imageHeight >> 1))+(y*imageHeight/350),width*imageWidth/250,height*imageHeight/350);
 	Gfx_updateScreen();
 }
 
