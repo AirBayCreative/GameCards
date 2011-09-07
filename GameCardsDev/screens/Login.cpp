fc3f79936ec614f88aff2cd83125bab11bbe3c0b
heinzs <heinz@mytcg.net>
2011-09-07 13:28:04 +0200

    notification changes

--- GameCardsDev/screens/Login.cpp
+++ GameCardsDev/screens/Login.cpp
@@ -415,19 +415,6 @@ void Login::mtxTagData(const char* data, int len) {
 		handle = data;
 	} else if(!strcmp(parentTag.c_str(), "freebie")) {
 		freebie = data;
-	} else if(!strcmp(parentTag.c_str(), "notedate")) {
-		notedate = data;
-		tm t;
-		t.tm_year = atoi(notedate.substr(0,4).c_str())-1900;
-		t.tm_mon = atoi(notedate.substr(5,2).c_str())-1;
-		t.tm_mday = atoi(notedate.substr(8,2).c_str());
-		t.tm_hour = atoi(notedate.substr(11,2).c_str());
-		t.tm_min = atoi(notedate.substr(14,2).c_str());
-		t.tm_sec = atoi(notedate.substr(17,2).c_str());
-		int ndate = mktime(&t);
-		if(ndate > atoi(feed->getNoteSeconds().c_str())){
-			feed->setNoteLoaded(true);
-		}
 	} else if(!strcmp(parentTag.c_str(), "error")) {
 		error_msg = data;
 	} else if (!strcmp(parentTag.c_str(), "result")) {
