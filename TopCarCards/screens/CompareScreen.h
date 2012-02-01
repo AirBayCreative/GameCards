fd567866ef6d5f22dfb7bb88ef2feae8eea89a5a
andrecronje <andre@mytcg.net>
2012-02-01 12:14:36 +0200

    topcar files
    Signed-off-by: andrecronje <andre@mytcg.net>

---
+++ TopCarCards/screens/CompareScreen.h
@@ -0,0 +1,64 @@
+#ifndef _COMPARESCREEN_H_
+#define _COMPARESCREEN_H_
+
+#include <MAUI/Screen.h>
+#include <MAUI/ListBox.h>
+#include <MAUI/Image.h>
+#include <maprofile.h>
+
+#include "../utils/Card.h"
+#include "../utils/Feed.h"
+#include "../utils/ImageCache.h"
+#include "../utils/XmlConnection.h"
+#include "../UI/Widgets/MobImage.h"
+
+using namespace MAUI;
+
+/**
+ *  The screen class used for demonstrating how to
+ *  use MAUI::Image widgets.
+ */
+class CompareScreen : public Screen, private XCListener, Mtx::XmlListener, private HttpConnectionListener {
+public:
+	CompareScreen(Screen *previous, MAHandle image, Feed *feed, bool flip, Card *card, Card *compare);
+	~CompareScreen();
+	void keyPressEvent(int keyCode);
+
+	void pointerPressEvent(MAPoint2d point);
+	void pointerMoveEvent(MAPoint2d point);
+	void pointerReleaseEvent(MAPoint2d point);
+	void locateItem(MAPoint2d point);
+
+	void clearListBox();
+private:
+	Screen *previous, *next;
+	MAHandle img;
+	MAPoint2d pointPressed, pointReleased;
+	Card *card, *compare;
+	Feed *feed;
+	MobImage *imge, *cmpge;
+	ImageCache *imageCache;
+	Layout *mainLayout;
+	ListBox *listBox;
+	int height, flipOrSelect, currentSelectedStat;
+	bool flip, busy;
+	bool left, right, list;
+	String parentTag;
+
+	HttpConnection mHttp;
+	XmlConnection xmlConn;
+
+	void httpFinished(MAUtil::HttpConnection*, int);
+	void connReadFinished(Connection*, int);
+	void xcConnError(int code);
+	void mtxEncoding(const char*);
+	void mtxTagStart(const char*, int);
+	void mtxTagAttr(const char*, const char*);
+	void mtxTagData(const char*, int);
+	void mtxTagEnd(const char*, int);
+	void mtxParseError(int);
+	void mtxEmptyTagEnd();
+	void mtxTagStartEnd();
+};
+
+#endif	//_COMPARESCREEN_H_
---
+++ TopCarCards/screens/ShopProductsScreen.h
@@ -0,0 +1,69 @@
+#ifndef _SHOPPRODUCTSSCREEN_H_
+#define _SHOPPRODUCTSSCREEN_H_
+
+#include <MAUI/Screen.h>
+#include <MAUI/Label.h>
+#include <maprofile.h>
+
+#include "../utils/XmlConnection.h"
+#include "../utils/Feed.h"
+#include "../utils/Product.h"
+#include "../utils/ImageCache.h"
+#include "../UI/KineticListBox.h"
+#include "../UI/Widgets/MobImage.h"
+
+using namespace MAUI;
+using namespace MAUtil;
+
+class ShopProductsScreen : public Screen, WidgetListener, private XCListener, Mtx::XmlListener, private HttpConnectionListener {
+public:
+	ShopProductsScreen(Screen *previous, Feed *feed, String category, bool free, bool first=false);
+	~ShopProductsScreen();
+	void keyPressEvent(int keyCode);
+	void selectionChanged(Widget *widget, bool selected);
+	void drawList();
+	void pop();
+	void clearListBox();
+
+	void pointerPressEvent(MAPoint2d point);
+	void pointerMoveEvent(MAPoint2d point);
+	void pointerReleaseEvent(MAPoint2d point);
+	void locateItem(MAPoint2d point);
+
+	typedef Vector<Product*> ProductVector;
+private:
+	Screen *next, *previous;
+	ImageCache *mImageCache;
+	MobImage *tempImage;
+	Label *notice, *label;
+	KineticListBox *listBox;
+	Layout *mainLayout;
+
+	HttpConnection mHttp;
+	XmlConnection xmlConn;
+
+	String parentTag,cardText;
+	String id, productType, productName, price, thumb, cardsInPack, category, cred;
+	int i, moved;
+	bool list, left, right, emp, free, credits, first;
+
+	Feed *feed;
+	ProductVector products;
+	Product *product;
+
+	void httpFinished(MAUtil::HttpConnection*, int);
+	void connReadFinished(Connection*, int);
+	void xcConnError(int code);
+	void mtxEncoding(const char*);
+	void mtxTagStart(const char*, int);
+	void mtxTagAttr(const char*, const char*);
+	void mtxTagData(const char*, int);
+	void mtxTagEnd(const char*, int);
+	void mtxParseError(int);
+	void mtxEmptyTagEnd();
+	void mtxTagStartEnd();
+
+	void clearProductsList();
+};
+
+#endif	//_SHOPPRODUCTSCREEN_H_*/
